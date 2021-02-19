fbdbac4e16ba82a20b287f0b6d15a3a7effb18b4
ShiJh <nnocreater@hotmail.com>
2021-02-19 16:54:53 +0800

    1

--- rename.py
+++ rename.py
@@ -17,32 +17,41 @@ path = "." + os.sep + "_posts"
 fileList = os.listdir(path)
 
 # 忽略特殊文件列表
-ignoreList = []
+ignoreList = [
+    r'.*.txt',
+    r'.*.bat'
+]
+
 
 # 获取文件中定义的日期
-
-
 def getDateStr(filePath: str) -> str:
     with open(filePath, 'r', encoding='utf-8') as file:
         content = file.read()
     try:
-        return re.search(r'(date: )(.*-.*-.*)', content).group(2)
+        return re.search(r'(date:)\s+(.*-.*-.*)', content).group(2)
     except:
         logging.error(filePath + " has not date text")
         sys.exit(-1)
 
+
+# 检测是否在排除列表中
+def ignored(name: str) -> bool:
+    for i in ignoreList:
+        if re.match(i, name) is not None:
+            return True
+
+    return False
+
+
 # 初始化logger
-
-
-def initlogger():
+def initLogger():
     logging.getLogger().setLevel(logging.INFO)
     logging.basicConfig(format='[%(levelname)s]: %(message)s')
 
 
 def main():
-    initlogger()
     for i in fileList:
-        if not ignoreList.__contains__(i):
+        if not ignored(i):
             oldname = path + os.sep + i
             timeStr = getDateStr(oldname)
             newname = path + os.sep + timeStr + '-' + i
@@ -53,11 +62,10 @@ def main():
                     newname = path + os.sep + timeStr + '-' + name
                 else:
                     newname = oldname
-
             os.rename(oldname, newname)
-
     logging.info('rename success')
 
 
 if __name__ == '__main__':
+    initLogger()
     main()
