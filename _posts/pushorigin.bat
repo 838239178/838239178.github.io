aa26baba08655deefbf4f26ef865f517d70a9151
ShiJh <nnocreater@hotmail.com>
2021-02-26 10:07:10 +0800

    回滚了命名方式

--- _posts/pushorigin.bat
+++ _posts/pushorigin.bat
@@ -1,9 +1,3 @@
-@echo off 
-if "%1" == "h" goto begin 
-
-　　mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)（window.close)&&exit 
-
-:begin
 git add .
 git pull origin main
 git commit -m "post update"
